851ade866d67db6621dec59be8f82ecdb4176af6
LLEEVV2020 <lleevv2020@gmail.com>
2022-11-14 17:03:42 +0400

    add

---
+++ src/js/main.js
@@ -0,0 +1,84 @@
+import './jquery.fancybox.js';
+
+import './_vendor';
+import vars from './_vars';
+import './_functions';
+import './_components';
+
+/**Не знаю куда добавить скипт для модалки */
+$(document).ready(function() {
+  $('.fancybox').fancybox({
+      padding : 0,
+      openEffect  : 'elastic',
+      closeBtn: false
+  });
+});
+
+/** слайждер */
+$('.responsive').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 6,
+  slidesToScroll: 6,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+/** слайждер */
+$('.certificates__slider').slick({
+  dots: true,
+  infinite: false,
+  arrows: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
