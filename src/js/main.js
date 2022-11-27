a9f0f94dfb2f8b543d1993f47a21b0331cc5699b
lleevv2030@gmail.com <lleevv2020@mail.com>
2022-11-27 10:12:36 +0400

    add

--- src/js/main.js
+++ src/js/main.js
@@ -523,6 +523,7 @@ function uploadFile(file){
 
 }
 
+/*
 $(document).ready(function() {
 
   const zeroLength = 2;
@@ -560,4 +561,96 @@ let timer = setInterval(function () {
     --timeMinut;
 }, 1000)
 
-});
+});*/
+
+
+        // класс для создание таймера обратного отсчета
+        class CountdownTimer {
+            constructor(deadline, cbChange, cbComplete) {
+                this._deadline = deadline;
+                this._cbChange = cbChange;
+                this._cbComplete = cbComplete;
+                this._timerId = null;
+                this._out = {
+                    days: '', hours: '', minutes: '', seconds: '',
+                    daysTitle: '', hoursTitle: '', minutesTitle: '', secondsTitle: ''
+                };
+                this._start();
+            }
+            static declensionNum(num, words) {
+                return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+            }
+            _start() {
+                this._calc();
+                this._timerId = setInterval(this._calc.bind(this), 1000);
+            }
+            _calc() {
+                const diff = this._deadline - new Date();
+                const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+                const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+                const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+                const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+                this._out.days = days < 10 ? days : days;
+                this._out.hours = hours < 10 ? '0' + hours : hours;
+                this._out.minutes = minutes < 10 ? '0' + minutes : minutes;
+                this._out.seconds = seconds < 10 ? '0' + seconds : seconds;
+                this._out.daysTitle = CountdownTimer.declensionNum(days, ['день', 'дня', 'дней']);
+                this._out.hoursTitle = CountdownTimer.declensionNum(hours, ['час', 'часа', 'часов']);
+                this._out.minutesTitle = CountdownTimer.declensionNum(minutes, ['минута', 'минуты', 'минут']);
+                this._out.secondsTitle = CountdownTimer.declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+                this._cbChange ? this._cbChange(this._out) : null;
+                if (diff <= 0) {
+                    clearInterval(this._timerId);
+                    this._cbComplete ? this._cbComplete() : null;
+                }
+            }
+        }
+
+        document.addEventListener('DOMContentLoaded', () => {
+
+            let hghhjhj = 13;
+
+            const runTimer = (vgggggg) => {
+                const elDays = document.querySelector('.timer__days');
+                const elHours = document.querySelector('.timer__hours');
+                const elMinutes = document.querySelector('.timer__minutes');
+                const elSeconds = document.querySelector('.timer__seconds');
+
+                const elDays2 = document.querySelector('.timer__days2');
+                const elHours2 = document.querySelector('.timer__hours2');
+                const elMinutes2 = document.querySelector('.timer__minutes2');
+                const elSeconds2 = document.querySelector('.timer__seconds2');
+
+
+                let deadline = Date.now();
+
+                hghhjhj = hghhjhj - vgggggg;
+
+                deadline = deadline + (new Date(deadline).getDay() - hghhjhj) * 86400000 + 604800000 - (60 * 1000) * 60 + 999;
+                deadline = new Date(deadline);
+                deadline.setHours(12, 0, 0, 0);
+
+                new CountdownTimer(deadline, (timer) => {
+                    elDays.textContent = timer.days % 7;
+                    elHours.textContent = timer.hours;
+                    elMinutes.textContent = timer.minutes;
+                    elSeconds.textContent = timer.seconds;
+
+
+                    elDays2.textContent = timer.days % 7;
+                    elHours2.textContent = timer.hours;
+                    elMinutes2.textContent = timer.minutes;
+                    elSeconds2.textContent = timer.seconds;
+                    /*elDays.dataset.title = timer.daysTitle;
+                    elHours.dataset.title = timer.hoursTitle;
+                    elMinutes.dataset.title = timer.minutesTitle;
+                    elSeconds.dataset.title = timer.secondsTitle;*/
+                }, () => {
+                    runTimer(7);
+                });
+            }
+            runTimer(0);
+        });
+
+
+
